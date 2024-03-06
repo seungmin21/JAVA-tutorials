b85035eaf17469c6ca0bb21a29829cfc42d72783
ohseungmin <ohmin2886@gmail.com>
2024-03-06 15:24:05 +0900

    자바 튜토리얼 진행

---
+++ Main.java
@@ -0,0 +1,14 @@
+// JAVA 클래스명과 파일명이 일치해야함
+// ; 세미콜론으로 항상 마무리를 해줘야함
+// string은 타입이 없다고 하고 대문자로 시작해야함
+// js는 console.log, python는 print, JAVA는 println으로 출력함
+// 대문자 시작은 관례라고 함
+
+public class Main {
+    public static void main(String[] args) {
+        String Greetings = "Hello";
+        String Language = " World!";
+    
+        System.out.println(Greetings + Language);
+    }
+}
